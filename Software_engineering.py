9a577d2639e130f87a992858ece23f7ee4ee0213
diegocurb <147955783+diegocurb@users.noreply.github.com>
2023-10-31 18:58:55 -0400

    Software_engineering.py

--- Software_engineering.py
+++ Software_engineering.py
@@ -8,6 +8,21 @@ def print_menu():
     option = input('Please enter an option: ')
     return option
 
+
+def encode(password):
+    password.split()
+    new_password = []
+    for num in password:
+        if int(num) >= 7:
+            num = (int(num) + 3) % 10
+        else:
+            num = int(num) + 3
+        new_password.append(str(num))
+    new_password = ''.join(new_password)
+    print('Your password has been encoded and stored!')
+    print(new_password)
+    return new_password
+
 def decode(secret):
     password = ""
     for i in secret:
@@ -15,3 +30,12 @@ def decode(secret):
         password += str(each_dig_password)
     print(password)
 
+option = '0'
+while option != '3':
+    option = print_menu()
+    if option == '1':
+        password = input('Please enter your password to encode: ')
+        encode(password)
+    elif option == '2':
+        password = input('Please enter your password to decode: ')
+        decode(password)
