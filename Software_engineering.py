b7ca4d4663d2808934f6ad57d3a4dc08fd84794b
diegocurb <147955783+diegocurb@users.noreply.github.com>
2023-10-31 19:16:30 -0400

    Update Software_engineering.py

--- Software_engineering.py
+++ Software_engineering.py
@@ -5,6 +5,7 @@ def print_menu():
     print('1. Encode')
     print('2. Decode')
     print('3. Quit')
+    print('')
     option = input('Please enter an option: ')
     return option
 
@@ -20,7 +21,7 @@ def encode(password):
         new_password.append(str(num))
     new_password = ''.join(new_password)
     print('Your password has been encoded and stored!')
-    print(new_password)
+    print('')
     return new_password
 
 def decode(secret):
@@ -28,14 +29,15 @@ def decode(secret):
     for i in secret:
         each_dig_password = (int(i) - 3) % 10
         password += str(each_dig_password)
-    print(password)
+    return password
 
 option = '0'
 while option != '3':
     option = print_menu()
     if option == '1':
-        password = input('Please enter your password to encode: ')
-        encode(password)
+        old_pass = input('Please enter your password to encode: ')
+        old_pass = encode(old_pass)
+        print(old_pass)
     elif option == '2':
-        password = input('Please enter your password to decode: ')
-        decode(password)
+        new_pass = decode(old_pass)
+        print(f'The encoded password is {old_pass}, and the original password is {new_pass}.')
